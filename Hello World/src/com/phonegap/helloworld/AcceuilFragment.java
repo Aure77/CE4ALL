930c883df3b4ae390e24b350db682acc5794e57b
hellone <manaakrouna@gmail.com>
2014-11-18 14:54:20 +0100

    MAJ

--- Hello World/src/com/phonegap/helloworld/AcceuilFragment.java
+++ Hello World/src/com/phonegap/helloworld/AcceuilFragment.java
@@ -2,12 +2,16 @@ package com.phonegap.helloworld;
 
 
 import org.apache.cordova.Config;
+import org.apache.cordova.CordovaChromeClient;
 import org.apache.cordova.CordovaWebView;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.webkit.WebView;
 
 public class AcceuilFragment extends CordovaFragmentAbstract {
 
@@ -15,22 +19,26 @@ public class AcceuilFragment extends CordovaFragmentAbstract {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+    	//getActivity().getWindow().requestFeature(Window.FEATURE_PROGRESS);
+    	final FragmentActivity activity = this.getActivity();
+    	
         LayoutInflater localInflater = inflater.cloneInContext(new CordovaContext(getActivity(), this));
         View rootView = localInflater.inflate(R.layout.my_cordova_frag, container, false);
         myWebView = (CordovaWebView) rootView.findViewById(R.id.myWebView);
         Config.init(getActivity());
         myWebView.getSettings().setJavaScriptEnabled(true);
-//        myWebView.loadUrl(Config.getStartUrl());
-        //myWebView.setWebChromeClient(new WebChromeClient());
-//        myWebView.setWebChromeClient(new WebChromeClient() {
-//        	   public void onProgressChanged(WebView view, int progress) {
-//        	     // Activities and WebViews measure progress with different scales.
-//        	     // The progress meter will automatically disappear when we reach 100%
-//        	     //activity.setProgress(progress * 1000);
-//        		   getActivity().setProgress(progress * 1000);
-//        	   }
-//        	 });
-     //   myWebView.setWebViewClient(new WebViewClient());
+        
+//        
+        myWebView.setWebChromeClient(new CordovaChromeClient(this) {
+     	   public void onProgressChanged(WebView view, int progress) {
+     	     // Activities and WebViews measure progress with different scales.
+     	     // The progress meter will automatically disappear when we reach 100%
+     	     //activity.setProgress(progress * 1000);
+     
+     		  activity.setProgress(progress * 1000);
+     	   }
+     	 });
+        	
 
         myWebView.loadUrl("file:///android_asset/www/html/home.html");
 
