cf1376de2742314ef4ce6d53eec4c7114b38e9a8
Aure77 <aure77@gmail.com>
2014-11-18 15:39:23 +0100

    Notification icon and vibration

--- Hello World/src/com/phonegap/helloworld/plugin/NotificationPlugin.java
+++ Hello World/src/com/phonegap/helloworld/plugin/NotificationPlugin.java
@@ -11,6 +11,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -59,10 +61,13 @@ public class NotificationPlugin extends CordovaPlugin {
 							String title = args.getString(1);
 							String message = args.getString(2);
 						
+							Bitmap icon = BitmapFactory.decodeResource(activity.getResources(), R.drawable.notif_icon_96);
 							NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(activity)
-									.setSmallIcon(R.drawable.icon_notif)
+									.setSmallIcon(R.drawable.notif_icon)
+									.setLargeIcon(icon)
 									.setContentTitle(title)
-									.setContentText(message);
+									.setContentText(message)
+									.setVibrate(new long[] { 200, 800, 200 });
 	
 							Intent viewIntent = new Intent(activity, HomeActivity.class);
 	
